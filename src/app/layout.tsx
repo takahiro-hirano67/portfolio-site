2f744c217dae8c38cb7b73d6123a337d4b8d107c
takahiro-hirano67 <hirano.takahiro715@gmail.com>
2026-06-04 19:55:37 +0900

    更新: RootLayoutコンポーネントにHeaderとFooterを追加し、子要素を適切に配置

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { notoSansJp, notoSerifJp } from "@/config/font-config";
 import { siteMetadata } from "@/config/metadata";
 import "./globals.css";
+import Footer from "@/components/common/Footer";
+import Header from "@/components/common/Header";
+import { HEADER_DATA } from "@/data/common-data";
 
 // メタデータをエクスポート
 export const metadata: Metadata = siteMetadata;
@@ -17,11 +20,14 @@ export default function RootLayout({
     return (
         <html
             lang="ja"
+            data-scroll-behavior="smooth" // 遷移時のみスムーススクロールを無効化 (競合するため)
             suppressHydrationWarning
             className={`${notoSansJp.variable} ${notoSerifJp.variable} h-full antialiased`}
         >
             <body suppressHydrationWarning={true} className="flex min-h-full flex-col font-sans">
-                {children}
+                <Header data={HEADER_DATA} />
+                <main className="flex-1 px-6 pt-42 pb-12">{children}</main>
+                <Footer />
             </body>
         </html>
     );
